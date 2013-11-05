b0d2f964af2549e9b69be608de7a494289850f0c
Ben Rudolph <rudolphben@gmail.com>
2013-11-05 15:32:54 +0700

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Visio::Application.config.secret_token = '969372bc91e17e50c430e7fc18016e438aa5f0d5ae585cf34afd8ddbd25b9c689bf370551b480f38ddd9c6dffc47f76fc4f78991e7fb2e034dcfb3355c57ed64'
