b815fd903690a3320b9ad4a98f3e6fba74ab1635
Ben Rudolph <rudolphben@gmail.com>
2013-11-14 17:38:02 +0100

    More detailed world map usage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  @@world = File.read("#{Rails.root}/public/world.json")
+  @@world = File.read("#{Rails.root}/public/world_50m.json")
 
   def index
     redirect_to :splash unless user_signed_in?
