9e927468c08a75b33d0db9e17260bfdc5abcf45f
Ben Rudolph <rudolphben@gmail.com>
2013-11-15 18:07:21 +0100

    Compile fonts in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Visio::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
