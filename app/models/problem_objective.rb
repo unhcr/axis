5a9fdf79e8a4a25bc59fbae4523986912e961ddb
Ben Rudolph <rudolphben@gmail.com>
2013-11-17 15:11:48 +0100

    Problem objective budget calculated

--- app/models/problem_objective.rb
+++ app/models/problem_objective.rb
@@ -8,4 +8,27 @@ class ProblemObjective < ActiveRecord::Base
   has_and_belongs_to_many :outputs, :uniq => true
   has_and_belongs_to_many :indicators, :uniq => true
   has_and_belongs_to_many :rights_groups, :uniq => true
+
+  AOL = 'Above Operating Level'
+  OL = 'Operating Level'
+
+  def aol_budget
+    unless @aol_budget
+      aol_budget_lines = self.budget_lines.where(:scenerio => AOL)
+      @aol_budget = aol_budget_lines.inject { |b1, b2| b1.amount + b2.amount } || 0
+    end
+    @aol_budget
+  end
+
+  def ol_budget
+    unless @ol_budget
+      ol_budget_lines = self.budget_lines.where(:scenerio => OL)
+      @ol_budget = ol_budget_lines.inject { |b1, b2| b1.amount + b2.amount } || 0
+    end
+    @ol_budget
+  end
+
+  def budget
+    return ol_budget + aol_budget
+  end
 end
