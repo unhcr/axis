b3d5f17c51ad0b24e0d0a33cf864b797b23eff90
Ben Rudolph <rudolphben@gmail.com>
2013-11-21 18:19:58 +0100

    public models in plan

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -12,4 +12,19 @@ class Plan < ActiveRecord::Base
   has_many :indicator_data
 
   belongs_to :operation
+
+  def self.public_models(plans, options = {})
+    response = Jbuilder.encode do |json|
+      json.plans plans do |plan|
+        json.(plan, :id, :name, :year)
+
+        json.ppg_ids plan.ppg_ids
+        json.goal_ids plan.goal_ids
+        json.problem_objective_ids plan.problem_objective_ids
+        json.indicator_ids plan.indicator_ids
+      end
+    end
+
+    return response
+  end
 end
