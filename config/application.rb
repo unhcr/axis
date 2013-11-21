964e0e931f8a2317eb9439a3bf36c26c2ad2f017
Ben Rudolph <rudolphben@gmail.com>
2013-11-22 00:33:12 +0100

    Always cause tmp file to be created from open-uri

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,10 @@ module Visio
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #
+    require 'open-uri'
+    OpenURI::Buffer.send :remove_const, 'StringMax' if OpenURI::Buffer.const_defined?('StringMax')
+    OpenURI::Buffer.const_set 'StringMax', 0
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
