09b4f334efb87d88abe9df27883d0d06ebd543ff
Ben Rudolph <rudolphben@gmail.com>
2013-11-22 11:51:21 +0100

    Tests for parameter sync in plan

--- app/helpers/parameter_sync.rb
+++ app/helpers/parameter_sync.rb
@@ -4,11 +4,11 @@ module Parameter
     models = {}
 
     if synced_date
-      models[:new] = self.where('created_at >= ?', synced_date).limit(limit)
-      models[:updated] = self.where('created_at < ? and updated_at >= ?', synced_date, synced_date).limit(limit)
+      models[:new] = self.where('created_at >= ? and is_deleted = false', synced_date).limit(limit)
+      models[:updated] = self.where('created_at < ? and updated_at >= ? and is_deleted = false', synced_date, synced_date).limit(limit)
       models[:deleted] = self.where('is_deleted = true and updated_at >= ?', synced_date).limit(limit)
     else
-      models[:new] = self.all.limit(limit)
+      models[:new] = self.limit(limit)
       models[:updated] = models[:deleted] = []
     end
 
