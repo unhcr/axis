2c5e1bc2a9fe06337feb64af7fc8f1b712aa8ac5
Ben Rudolph <rudolphben@gmail.com>
2013-11-25 13:23:00 -0500

    convert timestamp

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,7 +2,7 @@ class PlansController < ApplicationController
 
   def index
     year = params[:year] || Time.now.year
-    synced_date = params[:synced_date]
+    synced_date = params[:synced_timestamp] ? Time.at(params[:synced_timestamp].to_i) : nil
     options = {
       :include => {
         :counts => true
