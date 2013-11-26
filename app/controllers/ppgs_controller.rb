23ced6bd3e9585d1ce3b878b5a8d4acb18bf7351
Ben Rudolph <rudolphben@gmail.com>
2013-11-26 13:36:09 -0600

    Add index route to other params

--- app/controllers/ppgs_controller.rb
+++ app/controllers/ppgs_controller.rb
@@ -1,2 +1,13 @@
 class PpgsController < ApplicationController
+  def index
+    synced_date = params[:synced_timestamp] ? Time.at(params[:synced_timestamp].to_i) : nil
+
+    # Must be a nested route
+    if params[:plan_id]
+      plan = Plan.find(params[:plan_id])
+      render :json => plan.ppgs.as_json
+    else
+      render :json => Ppg.synced_models(synced_date, nil, 20, params[:where]).as_json
+    end
+  end
 end
