60a195db796acdae4058ca6d533ba511ad344a19
Ben Rudolph <rudolphben@gmail.com>
2013-12-03 16:00:26 +0100

    test for new impact indicators

--- test/unit/plan_test.rb
+++ test/unit/plan_test.rb
@@ -1,6 +1,19 @@
 require 'test_helper'
 
 class PlanTest < ActiveSupport::TestCase
+
+  test "get impact indicators" do
+    p = plans(:one)
+    p.indicators << [indicators(:one), indicators(:two)]
+    p.save
+
+    i = p.impact_indicators
+
+    assert_equal 1, i.length
+    assert_equal false, i[0].is_performance
+
+  end
+
   test "synced models no date" do
     p = [plans(:one), plans(:two)]
 
