7de15d04c3e7405cd8fd57dd5957c62d268f3eee
Ben Rudolph <rudolphben@gmail.com>
2013-12-03 16:30:13 +0100

    more tests for plans index

--- test/functional/plans_controller_test.rb
+++ test/functional/plans_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class PlansControllerTest < ActionController::TestCase
+  include AlgorithmHelper
+
   test 'plans index should get current plans if no year' do
     get :index
 
@@ -9,14 +11,8 @@ class PlansControllerTest < ActionController::TestCase
 
     assert_response :success
     assert_equal 1, plans.count
-    plans.each do |plan|
-      assert_equal plan["year"], Time.now.year
-      assert_equal 0, plan["indicators_count"]
-      assert_equal 0, plan["ppgs_count"]
-      assert_equal 0, plan["goals_count"]
-      assert_equal 0, plan["problem_objectives_count"]
-      assert_equal 0, plan["outputs_count"]
-    end
+    plan = plans[0]
+    assert_equal plan["year"], Time.now.year
 
   end
 
@@ -30,13 +26,57 @@ class PlansControllerTest < ActionController::TestCase
 
     assert_response :success
     assert_equal 1, plans.count
-    plans.each do |plan|
-      assert_equal plan["year"], 2012
-      assert_equal 0, plan["ppgs_count"]
-      assert_equal 0, plan["goals_count"]
-      assert_equal 0, plan["problem_objectives_count"]
-      assert_equal 0, plan["outputs_count"]
-    end
+    assert_equal plans[0]["year"], 2012
   end
 
+  test 'plans index should get counts' do
+    p = Plan.where(:year => 2012).first
+    p.ppgs << ppgs(:one)
+    p.save
+
+    get :index, { :year => 2012, :options => { :include => { :counts => true } } }
+
+
+    r = JSON.parse(response.body)
+    plans = r["new"]
+
+    assert_response :success
+    assert_equal 1, plans.count
+
+    plan = plans[0]
+
+    assert_equal plan["year"], 2012
+    assert_equal 1, plan["ppgs_count"]
+    assert_equal 0, plan["goals_count"]
+    assert_equal 0, plan["problem_objectives_count"]
+    assert_equal 0, plan["outputs_count"]
+  end
+
+  test 'plans index should get situation analysis' do
+
+    p = Plan.where(:year => 2012).first
+    p.indicators << indicators(:impact)
+
+    datum = indicator_data(:one)
+
+    p.indicator_data << datum
+    p.indicators[0].indicator_data << datum
+
+    p.indicators[0].save
+    p.save
+
+    get :index, { :year => 2012, :options => { :include => { :situation_analysis => true } } }
+
+
+    r = JSON.parse(response.body)
+    plans = r["new"]
+
+    assert_response :success
+    assert_equal 1, plans.count
+
+    plan = plans[0]
+
+    assert_equal AlgorithmHelper::ALGO_COLORS[:ok], plan["situation_analysis"]
+
+  end
 end
