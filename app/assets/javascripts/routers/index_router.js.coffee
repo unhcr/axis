f243475cdd9783796461bc299f086c5c36d9ac9b
Ben Rudolph <rudolphben@gmail.com>
2013-12-03 15:56:53 +0100

    frontend framework for using sit analy

--- app/assets/javascripts/routers/index_router.js.coffee
+++ app/assets/javascripts/routers/index_router.js.coffee
@@ -31,8 +31,14 @@ class Visio.Routers.IndexRouter extends Backbone.Router
     console.log 'index'
 
   setup: () ->
+
+    options =
+      options:
+        include:
+          counts: true
+          situation_analysis: true
     plans = Visio.manager.get('plans')
-    plans.fetchSynced().done(() =>
+    plans.fetchSynced(options).done(() =>
       Visio.manager.set('plans', plans)
       Visio.manager.getMap().done((map) =>
         @map.mapJSON(map)
--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -3,15 +3,10 @@ class PlansController < ApplicationController
   def index
     year = params[:year] || Time.now.year
     synced_date = params[:synced_timestamp] ? Time.at(params[:synced_timestamp].to_i) : nil
-    options = {
-      :include => {
-        :counts => true
-      }
-    }
 
     params[:where] ||= {}
     params[:where][:year] = year
 
-    render :json => Plan.synced_models(synced_date, params[:join_ids], nil, params[:where]).as_json(options)
+    render :json => Plan.synced_models(synced_date, params[:join_ids], nil, params[:where]).as_json(params[:options])
   end
 end
