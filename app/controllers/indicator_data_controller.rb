90d5f32ba34ba4e45fadd240d7e615a14471e06e
Ben Rudolph <rudolphben@gmail.com>
2013-12-04 12:39:51 +0100

    Proper syncing

--- app/controllers/indicator_data_controller.rb
+++ app/controllers/indicator_data_controller.rb
@@ -1,11 +1,12 @@
 class IndicatorDataController < ApplicationController
 
   def index
+    synced_date = params[:synced_timestamp] ? Time.at(params[:synced_timestamp].to_i) : nil
 
     if params[:strategy_id]
       strategy = Strategy.find(params[:strategy_id])
 
-      render :json => strategy.synced_data(params[:synced_date])
+      render :json => strategy.synced_data(synced_date)
     else
       render :json => { :new => [], :updated => [], :deleted => [] }
     end
