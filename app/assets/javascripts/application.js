f627cbbaddf03b8725b8813110eb05072d1fd096
Ben Rudolph <rudolphben@gmail.com>
2013-12-05 17:40:28 +0100

    have to require param first

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require backbone
 //= require visio
 //= require ./collections/parameter
+//= require ./models/parameter
 //= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
