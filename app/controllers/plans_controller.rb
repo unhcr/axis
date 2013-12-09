8bea647b42b3cf1ac128b392dfd023271de09b7d
Ben Rudolph <rudolphben@gmail.com>
2013-12-09 09:24:49 +0100

    optional year param

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,11 +1,14 @@
 class PlansController < ApplicationController
 
   def index
-    year = params[:year] || Time.now.year
+    year = params[:year]
     synced_date = params[:synced_timestamp] ? Time.at(params[:synced_timestamp].to_i) : nil
 
+
     params[:where] ||= {}
-    params[:where][:year] = year
+    if year
+      params[:where][:year] = year
+    end
 
     render :json => Plan.synced_models(synced_date, params[:join_ids], nil, params[:where]).as_json(params[:options])
   end
