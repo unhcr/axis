e0ff080217102a6e2593c31aec63c86cf659023e
Ben Rudolph <rudolphben@gmail.com>
2013-12-17 10:48:46 +0100

    Log instead of print

--- app/helpers/country_helper.rb
+++ app/helpers/country_helper.rb
@@ -2,7 +2,7 @@ module CountryHelper
 
   def match_plan_to_country(plan)
     country = Country.where("un_names LIKE ?", '%' + plan.operation_name + '%').first
-    p "No country for plan: #{plan.operation_name}" and return unless country
+    Rails.logger.info "No country for plan: #{plan.operation_name}" and return unless country
     plan.country = country
     plan.save
   end
