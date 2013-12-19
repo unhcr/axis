c782a1e04430b32668489f63b870403c4f26f7cf
Ben Rudolph <rudolphben@gmail.com>
2013-12-19 15:05:14 +0100

    (#130) - Setup hooks for SO

--- app/models/strategy.rb
+++ app/models/strategy.rb
@@ -1,7 +1,7 @@
 class Strategy < ActiveRecord::Base
   attr_accessible :name
 
-  has_many :strategy_objectives
+  has_many :strategy_objectives, :before_add => :add_strategy_objective_parameters
 
   has_and_belongs_to_many :operations, :uniq => true
   has_and_belongs_to_many :plans, :uniq => true
@@ -12,8 +12,16 @@ class Strategy < ActiveRecord::Base
   has_and_belongs_to_many :outputs, :uniq => true, :before_add => :belongs_to_strategy_objective
   has_and_belongs_to_many :indicators, :uniq => true, :before_add => :belongs_to_strategy_objective
 
+
+  def add_strategy_objective_parameters(strategy_objective)
+    self.goals << strategy_objective.goals
+    self.problem_objectives << strategy_objective.problem_objectives
+    self.outputs << strategy_objective.outputs
+    self.indicators << strategy_objective.indicators
+  end
+
   def belongs_to_strategy_objective(assoc)
-    raise 'Association does not belong to a Strategy Objective' if assoc.strategy_objectectives.empty?
+    raise 'Association does not belong to a Strategy Objective' if assoc.strategy_objectives.empty?
   end
 
   # Get IndicatorData related to particular strategy
--- app/models/strategy_objective.rb
+++ app/models/strategy_objective.rb
@@ -2,8 +2,24 @@ class StrategyObjective < ActiveRecord::Base
   attr_accessible :description, :name
   belongs_to :strategy
 
-  has_and_belongs_to_many :goals, :uniq => true
-  has_and_belongs_to_many :problem_objectives, :uniq => true
-  has_and_belongs_to_many :outputs, :uniq => true
-  has_and_belongs_to_many :indicators, :uniq => true
+  has_and_belongs_to_many :goals, :uniq => true,
+    :after_add => :add_to_strategy,
+    :after_remove => :remove_from_strategy
+  has_and_belongs_to_many :problem_objectives, :uniq => true,
+    :after_add => :add_to_strategy,
+    :after_remove => :remove_from_strategy
+  has_and_belongs_to_many :outputs, :uniq => true,
+    :after_add => :add_to_strategy,
+    :after_remove => :remove_from_strategy
+  has_and_belongs_to_many :indicators, :uniq => true,
+    :after_add => :add_to_strategy,
+    :after_remove => :remove_from_strategy
+
+  def add_to_strategy(assoc)
+    self.strategy.send(assoc.class.table_name) << assoc if self.strategy
+  end
+
+  def remove_from_strategy(assoc)
+    self.strategy.send(assoc.class.table_name).delete(assoc) if self.strategy
+  end
 end
