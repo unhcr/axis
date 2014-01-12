7d0a54feaea783dede9136f9d8c769c7a4794256
Ben Rudolph <rudolphben@gmail.com>
2014-01-12 13:51:36 +0100

    Remove old comment

--- app/controllers/problem_objectives_controller.rb
+++ app/controllers/problem_objectives_controller.rb
@@ -2,7 +2,6 @@ class ProblemObjectivesController < ApplicationController
   def index
     synced_date = params[:synced_timestamp] ? Time.at(params[:synced_timestamp].to_i) : nil
 
-    # Must be a nested route
     render :json => ProblemObjective.synced_models(synced_date, params[:join_ids], nil, params[:where]).as_json
   end
 end
