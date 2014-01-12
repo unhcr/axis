e7ed31aa6f451aa31b7490841e6d1137e5804c2e
Ben Rudolph <rudolphben@gmail.com>
2014-01-12 19:51:58 +0100

    Index function for syncable parameter with tests

--- app/helpers/syncable_parameter.rb
+++ app/helpers/syncable_parameter.rb
@@ -4,7 +4,17 @@ module SyncableParameter
   def synced
     synced_date = params[:synced_timestamp] ? Time.at(params[:synced_timestamp].to_i) : nil
 
-    render :json => resource.synced_models(synced_date, params[:join_ids], nil, params[:where]).as_json(params[:options])
+    render :json => resource.synced_models(
+      synced_date,
+      params[:join_ids],
+      nil,
+      params[:where]).as_json(params[:options])
+  end
+
+  def index
+    results = resource.where(params[:where])
+    results = params[:page].present? ? results.page(params[:page]) : results.all
+    render :json => results.as_json(params[:options])
   end
 end
 
--- test/functional/plans_controller_test.rb
+++ test/functional/plans_controller_test.rb
@@ -63,6 +63,35 @@ class PlansControllerTest < ActionController::TestCase
     plan = plans[0]
 
     assert_equal IndicatorDatum::ALGO_RESULTS[:ok], plan["situation_analysis"]["category"]
+  end
 
+  test 'index should get all plans' do
+    get :index
+
+    plans = JSON.parse(response.body)
+
+    assert_response :success
+    assert_equal 3, plans.count
+  end
+
+  test 'index should get all 2012 plans' do
+    get :index, { :where => { :year => 2012 } }
+
+    plans = JSON.parse(response.body)
+
+    assert_response :success
+    assert_equal 1, plans.count
+    assert_equal plans[0]["year"], 2012
+  end
+
+  test 'index should get one page of plans' do
+    Plan.per_page = 1
+
+    get :index, { :page => 1 }
+
+    plans = JSON.parse(response.body)
+
+    assert_response :success
+    assert_equal 1, plans.count
   end
 end
