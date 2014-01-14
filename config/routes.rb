e088b9b954db9082d60546e77ac02bd54750e8ec
Ben Rudolph <rudolphben@gmail.com>
2014-01-14 12:00:55 +0100

    Add indicator data routes back in

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Visio::Application.routes.draw do
   syncable_resources :problem_objectives
   syncable_resources :outputs
   syncable_resources :indicators
+  syncable_resources :indicator_data
   syncable_resources :budgets
   resources :strategies
   resources :users
