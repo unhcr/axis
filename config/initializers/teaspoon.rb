d15c6f02d66f84beb40d9bc3422eb7f75e82d80e
Ben Rudolph <rudolphben@gmail.com>
2014-01-22 14:18:50 +0100

    Only setup teaspoon in dev and test

--- config/initializers/teaspoon.rb
+++ config/initializers/teaspoon.rb
@@ -1,9 +1,11 @@
-Teaspoon.setup do |config|
+if Rails.env.test? || Rails.env.development?
+  Teaspoon.setup do |config|
 
-  config.suite do |suite|
-    suite.matcher = "test/javascripts/**/*_test.{js,js.coffee,coffee}"
-    suite.javascripts = ["teaspoon/qunit"]
-    suite.helper = "test_helper"
+    config.suite do |suite|
+      suite.matcher = "test/javascripts/**/*_test.{js,js.coffee,coffee}"
+      suite.javascripts = ["teaspoon/qunit"]
+      suite.helper = "test_helper"
+    end
+
   end
-
 end
