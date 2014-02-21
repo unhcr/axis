d50e09c0a91b17fa334d67089f58601391c0635e
Ben Rudolph <rudolphben@gmail.com>
2014-02-21 16:17:02 +0100

    Precompile cms.css

--- config/application.rb
+++ config/application.rb
@@ -79,7 +79,7 @@ module Visio
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.precompile += ['cms.js']
+    config.assets.precompile += ['cms.js', 'cms.css']
 
   end
 end
