d9c0ce1f39a14ba5692bcc2a656c35e8a80cdc29
Ben Rudolph <rudolphben@gmail.com>
2014-03-10 18:18:19 +0100

    Don't compress javascript in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,8 +13,15 @@ Visio::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  class NoCompression
+    def compress(string)
+      string
+    end
+  end
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.js_compressor = NoCompression.new
+  config.assets.css_compressor = NoCompression.new
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
