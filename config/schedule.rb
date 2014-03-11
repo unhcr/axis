9219128765df9fd9a337c0d4599b6936f7d70eea
Ben Rudolph <rudolphben@gmail.com>
2014-03-11 11:20:52 +0100

    Set env variables in cron task

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,11 @@
 # Use this file to easily define all of your cron jobs.
 
 set :output, "./log/cron.log"
+env :PATH, '$HOME/.rbenv/bin:$PATH'
+env :http_proxy, 'http://proxy.unhcr.local:8080'
+env :https_proxy, 'https://proxy.unhcr.local:8080'
 
-every :day, :at => '4:30 am' do
-  command 'source ~/.bashrc'
+every 10.minutes, :at => '4:30 am' do
   rake 'build:focus'
   rake 'build:countries'
   rake 'build:msrp'
