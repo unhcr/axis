613729e4480599df80ebb3ee273c8ad025530fa4
Ben Rudolph <rudolphben@gmail.com>
2014-03-14 11:52:37 +0100

    (#368) - Add update route
    Export module was attempting to save without update route.

--- app/controllers/export_modules_controller.rb
+++ app/controllers/export_modules_controller.rb
@@ -9,6 +9,16 @@ class ExportModulesController < ApplicationController
     render :json => export_module
   end
 
+  def update
+    export_module = current_user.export_modules.find(params[:id])
+    render :json => {} unless export_module
+
+    export_module.update_attributes params[:export_module]
+    export_module.save
+
+    render :json => export_module
+  end
+
   def index
     render :json => current_user.export_modules
   end
--- test/functional/export_modules_controller_test.rb
+++ test/functional/export_modules_controller_test.rb
@@ -11,7 +11,7 @@ class ExportModulesControllerTest < ActionController::TestCase
   end
 
   test 'export module create' do
-    get :create, { :export_module =>
+    post :create, { :export_module =>
       {
         :title => 'title',
         :description => 'descrip',
@@ -32,6 +32,39 @@ class ExportModulesControllerTest < ActionController::TestCase
 
     r = JSON.parse(response.body)
     m = ExportModule.find(r['id'])
+
+    assert_equal '2012', m.state['year'], 'Year should be 2012'
+    assert_equal 'a type', m.state['amount_type'], 'Amount type should "a type"'
+    assert m.state['selected']['plan_ids']['abc'], 'plan_id abc should be true'
+    assert_equal '1', m.figure_config['data'][0]['value']
+    assert_equal m.user.id, @user.id, 'Should belong to the user signed in'
+    assert_equal m.include_parameter_list, false, 'Default parameter list value'
+    assert_equal m.include_explaination, false, 'Default explaination value'
+  end
+
+  test 'export module update' do
+    post :create, {}
+    r = JSON.parse(response.body)
+    m = ExportModule.find(r['id'])
+
+    post :update, { :id => m.id, :export_module =>
+      {
+        :title => 'title',
+        :description => 'descrip',
+        :state => { :year => 2012,
+                    :amount_type => 'a type',
+                    :selected => {
+                      :plan_ids => {
+                        'abc' => true
+                      }
+
+                    }
+                  },
+        :figure_config => { :data => [{ :value => 1 }] }
+      }
+    }
+
+    m.reload
 
     assert_equal '2012', m.state['year'], 'Year should be 2012'
     assert_equal 'a type', m.state['amount_type'], 'Amount type should "a type"'
