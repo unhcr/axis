07332a51e62574509dfd9ab10ef099911770374f
Ben Rudolph <rudolphben@gmail.com>
2014-04-14 11:24:39 +0200

    Revert "Longer cache ttl"
    This reverts commit a27476a54deacae1ce6af0de3cb0de73c8ac0c6b.

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Visio
     OpenURI::Buffer.const_set 'StringMax', 0
 
     require 'shrimp'
-    config.middleware.use Shrimp::Middleware, :cache_ttl => 1000, :out_path => "#{Rails.root}/public/reports/pdf", :polling_interval => 4
+    config.middleware.use Shrimp::Middleware, :cache_ttl => 0, :out_path => "#{Rails.root}/public/reports/pdf", :polling_interval => 4
 
     Shrimp.configure do |config|
       config.viewport_width = 896
