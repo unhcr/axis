5cd74ae27a8a50e98d9aa6a016c034482b1af153
Ben Rudolph <rudolphben@gmail.com>
2014-06-05 09:30:04 +0200

    Custom ANT path for development

--- app/helpers/build/ant_build.rb
+++ app/helpers/build/ant_build.rb
@@ -72,7 +72,8 @@ module Build
 
       Dir.chdir "#{self.build_path}"
 
-      cmd = "ant #{@config[:build_name]}"
+      # Need 1.9.4 version in order have file encoding
+      cmd = "#{ENV['ant']} #{@config[:build_name]}"
 
       log "Running '#{cmd}' in #{self.build_path}"
       output = system(cmd)
