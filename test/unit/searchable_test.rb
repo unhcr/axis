f4d46ba7b6edabbe6bb8f022f6507cf6e093a6aa
Ben Rudolph <rudolphben@gmail.com>
2014-06-24 17:36:44 +0200

    searchable test fix?

--- test/unit/searchable_test.rb
+++ test/unit/searchable_test.rb
@@ -4,7 +4,18 @@ class SearchableTest < ActiveSupport::TestCase
 
   def setup
     @searchables = [Operation, Ppg, Goal, Output, ProblemObjective, Indicator]
+    @searchables.each do |searchable|
+      # Have to delete index before each test
+      searchable.index_name('test_' + searchable.model_name.plural)
+      searchable.index.delete
+      searchable.create_elasticsearch_index
 
+      one = self.send(searchable.table_name, :one)
+      two = self.send(searchable.table_name, :two)
+      searchable.index.import [one, two]
+      searchable.index.refresh
+
+    end
   end
 
   test 'paged search max results' do
