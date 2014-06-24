837cf5ed3d1874cea10f5f7042c0133328dbad74
Ben Rudolph <rudolphben@gmail.com>
2014-06-24 09:59:19 +0200

    formatting with sql

--- data/queries/narratives.sql
+++ data/queries/narratives.sql
@@ -1,16 +1,16 @@
 select * from (
 -- Elements for plan
 select bi_operation.operationid,
-        bi_plan.planid as planid,
+        bi_plan.planid AS planid,
         bi_plan.PLANNINGYEAR,
-        null as ppgid,
-        null as rfppgid,
-        null as goalid,
-        null as rfgoalid,
-        null as objid,
-        null as RFPROBLEMOBJECTIVEID,
-        null as outputid,
-        null as rfoutputid,
+        null AS ppgid,
+        null AS rfppgid,
+        null AS goalid,
+        null AS rfgoalid,
+        null AS objid,
+        null AS RFPROBLEMOBJECTIVEID,
+        null AS outputid,
+        null AS rfoutputid,
         bi_narrativereports.eltid,
         bi_narrativereports.planeltype,
         replace(bi_narrativereports.usertxt, chr(10), '\n'),
@@ -20,22 +20,22 @@ select bi_operation.operationid,
 
 from bi_operation
 INNER JOIN BI_PLAN                ON   BI_OPERATION.OPERATIONID=BI_PLAN.OPERATIONID
-inner join bi_narrativereports on bi_narrativereports.eltid = bi_plan.planid
+INNER JOIN bi_narrativereports on bi_narrativereports.eltid = bi_plan.planid
 
-union all
+UNION ALL
 
 -- Elements for objective
 select bi_operation.operationid,
-        bi_plan.planid as planid,
+        bi_plan.planid AS planid,
         bi_plan.PLANNINGYEAR,
-        null as ppgid,
-        BI_PRJPOPULATIONGROUP.ORIGPOPGROUP_ID as rfppgid,
-        null as goalid,
-        bi_goal.rfgoalid as rfgoalid,
-        bi_objective.objectiveid as objid,
-        bi_objective.RFPROBLEMOBJECTIVEID as RFPROBLEMOBJECTIVEID,
-        null as outputid,
-        null as rfoutputid,
+        null AS ppgid,
+        BI_PRJPOPULATIONGROUP.ORIGPOPGROUP_ID AS rfppgid,
+        null AS goalid,
+        bi_goal.rfgoalid AS rfgoalid,
+        bi_objective.objectiveid AS objid,
+        bi_objective.RFPROBLEMOBJECTIVEID AS RFPROBLEMOBJECTIVEID,
+        null AS outputid,
+        null AS rfoutputid,
         bi_narrativereports.eltid,
         bi_narrativereports.planeltype,
         replace(bi_narrativereports.usertxt, chr(10), '\n'),
@@ -51,22 +51,22 @@ INNER JOIN BI_RFGOALS             ON BI_GOAL.RFGOALID = BI_RFGOALS.ID
 INNER JOIN BI_RIGHTSGROUP         ON  BI_GOAL.GOALID=BI_RIGHTSGROUP.GOALID
 INNER JOIN BI_RFRIGHTSGROUP       ON BI_RIGHTSGROUP.RFRIGHTSGROUPID = BI_RFRIGHTSGROUP.ID
 INNER JOIN BI_OBJECTIVE           ON  BI_RIGHTSGROUP.RIGHTSGROUPID=BI_OBJECTIVE.RIGHTSGROUPID
-inner join bi_narrativereports on bi_narrativereports.eltid = bi_objective.objectiveid
+INNER JOIN bi_narrativereports on bi_narrativereports.eltid = bi_objective.objectiveid
 
-union all
+UNION ALL
 
 -- Elements for problem
 select bi_operation.operationid,
-        bi_plan.planid as planid,
+        bi_plan.planid AS planid,
         bi_plan.PLANNINGYEAR,
-        null as ppgid,
-        BI_PRJPOPULATIONGROUP.ORIGPOPGROUP_ID as rfppgid,
-        null as goalid,
-        bi_goal.rfgoalid as rfgoalid,
-        bi_objective.problemid as objid,
-        bi_objective.RFPROBLEMOBJECTIVEID as RFPROBLEMOBJECTIVEID,
-        null as outputid,
-        null as rfoutputid,
+        null AS ppgid,
+        BI_PRJPOPULATIONGROUP.ORIGPOPGROUP_ID AS rfppgid,
+        null AS goalid,
+        bi_goal.rfgoalid AS rfgoalid,
+        bi_objective.problemid AS objid,
+        bi_objective.RFPROBLEMOBJECTIVEID AS RFPROBLEMOBJECTIVEID,
+        null AS outputid,
+        null AS rfoutputid,
         bi_narrativereports.eltid,
         bi_narrativereports.planeltype,
         replace(bi_narrativereports.usertxt, chr(10), '\n'),
@@ -81,22 +81,22 @@ INNER JOIN BI_RFGOALS             ON BI_GOAL.RFGOALID = BI_RFGOALS.ID
 INNER JOIN BI_RIGHTSGROUP         ON  BI_GOAL.GOALID=BI_RIGHTSGROUP.GOALID
 INNER JOIN BI_RFRIGHTSGROUP       ON BI_RIGHTSGROUP.RFRIGHTSGROUPID = BI_RFRIGHTSGROUP.ID
 INNER JOIN BI_OBJECTIVE           ON  BI_RIGHTSGROUP.RIGHTSGROUPID=BI_OBJECTIVE.RIGHTSGROUPID
-inner join bi_narrativereports on bi_narrativereports.eltid = bi_objective.problemid
+INNER JOIN bi_narrativereports on bi_narrativereports.eltid = bi_objective.problemid
 
-union all
+UNION ALL
 
 -- Elements for goals
 select bi_operation.operationid,
-        bi_plan.planid as planid,
+        bi_plan.planid AS planid,
         bi_plan.PLANNINGYEAR,
-        null as ppgid,
-        BI_PRJPOPULATIONGROUP.ORIGPOPGROUP_ID as rfppgid,
-        BI_GOAL.GOALID as goalid,
-        BI_GOAL.RFGOALID as rfgoalid,
-        null as objid,
-        null as RFPROBLEMOBJECTIVEID,
-        null as outputid,
-        null as rfoutputid,
+        null AS ppgid,
+        BI_PRJPOPULATIONGROUP.ORIGPOPGROUP_ID AS rfppgid,
+        BI_GOAL.GOALID AS goalid,
+        BI_GOAL.RFGOALID AS rfgoalid,
+        null AS objid,
+        null AS RFPROBLEMOBJECTIVEID,
+        null AS outputid,
+        null AS rfoutputid,
         bi_narrativereports.eltid,
         bi_narrativereports.planeltype,
         replace(bi_narrativereports.usertxt, chr(10), '\n'),
@@ -108,22 +108,22 @@ INNER JOIN BI_PLAN                ON   BI_OPERATION.OPERATIONID=BI_PLAN.OPERATIO
 INNER JOIN BI_PRJPOPULATIONGROUP  ON  BI_PLAN.PLANID=BI_PRJPOPULATIONGROUP.PLANID
 INNER JOIN BI_GOAL                ON  BI_PRJPOPULATIONGROUP.POPULATIONGROUP_ID=BI_GOAL.POPULATIONGROUPID
 INNER JOIN BI_RFGOALS             ON BI_GOAL.RFGOALID = BI_RFGOALS.ID
-inner join bi_narrativereports on bi_narrativereports.eltid = bi_goal.goalid
+INNER JOIN bi_narrativereports on bi_narrativereports.eltid = bi_goal.goalid
 
-union all
+UNION ALL
 
 -- Elements for PPGs
 select bi_operation.operationid,
-        bi_plan.planid as planid,
+        bi_plan.planid AS planid,
         bi_plan.PLANNINGYEAR,
-        BI_PRJPOPULATIONGROUP.populationgroup_id as ppgid,
-        BI_PRJPOPULATIONGROUP.ORIGPOPGROUP_ID as rfppgid,
-        null as goalid,
-        null as rfgoalid,
-        null as objid,
-        null as RFPROBLEMOBJECTIVEID,
-        null as outputid,
-        null as rfoutputid,
+        BI_PRJPOPULATIONGROUP.populationgroup_id AS ppgid,
+        BI_PRJPOPULATIONGROUP.ORIGPOPGROUP_ID AS rfppgid,
+        null AS goalid,
+        null AS rfgoalid,
+        null AS objid,
+        null AS RFPROBLEMOBJECTIVEID,
+        null AS outputid,
+        null AS rfoutputid,
         bi_narrativereports.eltid,
         bi_narrativereports.planeltype,
         replace(bi_narrativereports.usertxt, chr(10), '\n'),
@@ -133,22 +133,22 @@ select bi_operation.operationid,
 from bi_operation
 INNER JOIN BI_PLAN                ON   BI_OPERATION.OPERATIONID=BI_PLAN.OPERATIONID
 INNER JOIN BI_PRJPOPULATIONGROUP  ON  BI_PLAN.PLANID=BI_PRJPOPULATIONGROUP.PLANID
-inner join bi_narrativereports on bi_narrativereports.eltid = BI_PRJPOPULATIONGROUP.populationgroup_id
+INNER JOIN bi_narrativereports on bi_narrativereports.eltid = BI_PRJPOPULATIONGROUP.populationgroup_id
 
-union all
+UNION ALL
 
 -- Elements for outputs
 select bi_operation.operationid,
-        bi_plan.planid as planid,
+        bi_plan.planid AS planid,
         bi_plan.PLANNINGYEAR,
-        null as ppgid,
-        BI_PRJPOPULATIONGROUP.ORIGPOPGROUP_ID as rfppgid,
-        null as goalid,
-        bi_goal.rfgoalid as rfgoalid,
-        null as objid,
-        bi_objective.RFPROBLEMOBJECTIVEID as RFPROBLEMOBJECTIVEID,
-        null as outputid,
-        BI_OUTPUTGRP.RFOUTPUTID as rfoutputid,
+        null AS ppgid,
+        BI_PRJPOPULATIONGROUP.ORIGPOPGROUP_ID AS rfppgid,
+        null AS goalid,
+        bi_goal.rfgoalid AS rfgoalid,
+        null AS objid,
+        bi_objective.RFPROBLEMOBJECTIVEID AS RFPROBLEMOBJECTIVEID,
+        null AS outputid,
+        BI_OUTPUTGRP.RFOUTPUTID AS rfoutputid,
         bi_narrativereports.eltid,
         bi_narrativereports.planeltype,
         replace(bi_narrativereports.usertxt, chr(10), '\n'),
@@ -164,6 +164,6 @@ INNER JOIN BI_RIGHTSGROUP         ON  BI_GOAL.GOALID=BI_RIGHTSGROUP.GOALID
 INNER JOIN BI_RFRIGHTSGROUP       ON BI_RIGHTSGROUP.RFRIGHTSGROUPID = BI_RFRIGHTSGROUP.ID
 INNER JOIN BI_OBJECTIVE           ON  BI_RIGHTSGROUP.RIGHTSGROUPID=BI_OBJECTIVE.RIGHTSGROUPID
 INNER JOIN BI_OUTPUTGRP          ON BI_OBJECTIVE.OBJECTIVEID=BI_OUTPUTGRP.OBJECTIVEID
-inner join bi_narrativereports on bi_narrativereports.eltid = BI_OUTPUTGRP.outputid) t
+INNER JOIN bi_narrativereports on bi_narrativereports.eltid = BI_OUTPUTGRP.outputid) t
 
 WHERE PLANNINGYEAR >= 2012
