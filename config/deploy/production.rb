4ba3f5cd5e2f8049206e3fd789a535258bf5447b
Chrislain Razafimahefa <razafima@gmail.com>
2016-05-03 15:12:44 +0200

    Makes production config files deploy on staging for now.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
-server = '10.9.43.153'
+#server = '10.9.43.153' # production
+server = '10.9.43.240' # staging
 
 role :web, server
 role :app, server                          # This may be the same as your `Web` server
 role :db,  server, :primary => true # This is where Rails migrations will run
 role :resque_worker, server
-
