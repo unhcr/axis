c39bf784f694bef50f4bcfd355e3ca46ea597079
Chrislain Razafimahefa <razafima@gmail.com>
2017-09-18 11:43:54 +0200

    Makes ant work again.

--- config/initializers/ant.rb
+++ config/initializers/ant.rb
@@ -1,6 +1,6 @@
 if Rails.env.development?
-  ENV['ant'] = '/usr/local/Cellar/ant/1.9.4/bin/ant'
+  # ENV['ant'] = '/usr/local/Cellar/ant/1.9.4/bin/ant'
+  ENV['ant'] = 'ant' # Works with ant version 1.10.1
 else
   ENV['ant'] = 'ant'
 end
-
