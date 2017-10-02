ccc8773b2b5021accb32afefd574054c6895bcea
Chrislain Razafimahefa <razafima@gmail.com>
2017-10-02 17:35:28 +0200

    Restores sending email to their recipients.

--- app/resque_jobs/email_phantom_job.rb
+++ app/resque_jobs/email_phantom_job.rb
@@ -31,7 +31,7 @@ module EmailPhantomJob
 
       Rails.logger.info "XXXX: pdf generated: #{fullpath}"
 
-      Pony.mail(:to => toxs,
+      Pony.mail(:to => to,
                 :from => 'axis@unhcr.org',
                 :subject => name,
                 :body => Quoth.get,
