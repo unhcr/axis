c6d05120aa9d43effe544798619dbf20684f426e
Chrislain Razafimahefa <razafima@gmail.com>
2017-10-03 16:33:45 +0200

    Removes comment.

--- app/resque_jobs/email_phantom_job.rb
+++ app/resque_jobs/email_phantom_job.rb
@@ -29,8 +29,6 @@ module EmailPhantomJob
       p = Shrimp::Phantom.new(url, @options, cookies)
       fullpath = p.to_pdf(path)
 
-#to = 'razafima@unhcr.org'
-
       Rails.logger.info "XXXX: pdf generated: #{fullpath}. Sending email to: #{to}"
 
       Pony.mail(:to => to,
